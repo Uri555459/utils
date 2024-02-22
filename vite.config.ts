efdc765f9ce49c6430d577aa52dfb9c1394d9028
Uri <uri555459@gmail.com>
2024-02-22 12:47:59 +0300

    Added alias vite.config.js and modified .prettierrc file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()]
+	plugins: [react()],
+	resolve: {
+		alias: {
+			// eslint-disable-next-line no-undef
+			'@': path.resolve(__dirname, './src')
+		}
+	}
 })

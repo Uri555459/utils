75f3831364a2dd0eaabcaeb5ffb74bf8c5b9edbf
Uri <uri555459@gmail.com>
2024-02-22 09:54:07 +0300

    Added command prettier in package.json

--- src/pages/Home/Home.lazy.tsx
+++ src/pages/Home/Home.lazy.tsx
@@ -1,11 +1,13 @@
-import React, { lazy, Suspense } from 'react';
+import React, { Suspense, lazy } from 'react';
 
 const LazyHome = lazy(() => import('./Home'));
 
-const Home = (props: JSX.IntrinsicAttributes & { children?: React.ReactNode; }) => (
-  <Suspense fallback={null}>
-    <LazyHome {...props} />
-  </Suspense>
+const Home = (
+	props: JSX.IntrinsicAttributes & { children?: React.ReactNode }
+) => (
+	<Suspense fallback={null}>
+		<LazyHome {...props} />
+	</Suspense>
 );
 
 export default Home;
